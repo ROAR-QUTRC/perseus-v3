6b213e067980344be6080ad00181a757bc84833a
huon <huonswales@gmail.com>
2026-07-17 13:57:06 +1000

    Feat: setup web-ui profile

--- devenv.nix
+++ devenv.nix
@@ -7,7 +7,7 @@
   # nixpkgs,
   nix-ros-overlay,
   nix-ros-workspace,
-  nixgl,
+  # nixgl,
   ...
 }:
 
@@ -45,15 +45,4 @@ in
       livox-sdk2
     ]
     ++ flattenDerivationSet examples;
-
-  enterShell = ''
-    echo -e "\e[38;5;208m______                                    _____ ";
-    echo -e "| ___ \\                                  |____ |";
-    echo -e "| |_/ /__ _ __ ___  ___ _   _ ___  __   __   / /";
-    echo -e "|  __/ _ \\ '__/ __|/ _ \\ | | / __| \\ \\ / /   \\ \\";
-    echo -e "| | |  __/ |  \\__ \\  __/ |_| \\__ \\  \\ V /.___/ /";
-    echo -e "\\_|  \\___|_|  |___/\\___|\\__,_|___/   \\_/ \\____/ ";
-    # echo -e "------------------------------------------------";
-    echo -e "QUTRC - Remote Off-world Autonomous Robotics\e[0m";
-  '';
 }
