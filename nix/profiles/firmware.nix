50d6d158c44443f3420f24c0ee3607f07777f2d0
huon <huonswales@gmail.com>
2026-07-22 16:25:56 +1000

    Fix: removed platform io plugin and python

--- nix/profiles/firmware.nix
+++ nix/profiles/firmware.nix
@@ -1,12 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   packages = with pkgs; [
     platformio
     avrdude
     esptool
     espflash
   ];
-
-  # languages.python.enable = true;
 
   enterShell = ''
     cd $DEVENV_ROOT/firmware/excavation-bucket
