8db4f9f6f84b5609ba8d63983d9231ac25ed3edf
bocho0600 <nguyenleminh1002@gmail.com>
2026-08-12 21:36:59 +1000

    Chore: Update Nix packaging

--- nix/extra-packages/ros-packages/perseus/overlay.nix
+++ nix/extra-packages/ros-packages/perseus/overlay.nix
@@ -1,12 +1,16 @@
-final: prev: {
-  autonomy = final.callPackage ./autonomy.nix { };
-  perseus = final.callPackage ./perseus.nix { };
-  perseus-can-if = final.callPackage ./perseus-can-if.nix { };
-  perseus-description = final.callPackage ./perseus-description.nix { };
-  perseus-hardware = final.callPackage ./perseus-hardware.nix { };
-  perseus-interfaces = final.callPackage ./perseus-interfaces.nix { };
-  perseus-payloads = final.callPackage ./perseus-payloads.nix { };
-  perseus-sensors = final.callPackage ./perseus-sensors.nix { };
-  perseus-teleop = final.callPackage ./perseus-teleop.nix { };
-  perseus-vision = final.callPackage ./perseus-vision.nix { };
+final: prev:
+{
+  autonomy-bringup = final.callPackage ./autonomy-bringup.nix {};
+  footprint-broadcaster = final.callPackage ./footprint-broadcaster.nix {};
+  global-traversability = final.callPackage ./global-traversability.nix {};
+  perseus = final.callPackage ./perseus.nix {};
+  perseus-can-if = final.callPackage ./perseus-can-if.nix {};
+  perseus-description = final.callPackage ./perseus-description.nix {};
+  perseus-hardware = final.callPackage ./perseus-hardware.nix {};
+  perseus-interfaces = final.callPackage ./perseus-interfaces.nix {};
+  perseus-payloads = final.callPackage ./perseus-payloads.nix {};
+  perseus-sensors = final.callPackage ./perseus-sensors.nix {};
+  perseus-teleop = final.callPackage ./perseus-teleop.nix {};
+  perseus-vision = final.callPackage ./perseus-vision.nix {};
+  watchdog = final.callPackage ./watchdog.nix {};
 }
