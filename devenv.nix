d10787298c7e8bd5550ddf92e9b45ff07ff14902
huon <huonswales@gmail.com>
2026-09-01 20:00:19 +1000

    Feat: custom bash prompt

--- devenv.nix
+++ devenv.nix
@@ -41,4 +41,21 @@ in
     # Needs to be a derivation because devenv doesn't like pkgs.lib.recurseIntoAttrs for some reason
     pkgs = pkgs.runCommand "roar-all-pkgs" { passthru = pkgs; } "touch $out";
   };
+
+  # Customize the the command prompt in the shell
+  enterShell = ''
+    # get the profile name
+    dotfile="''${DEVENV_DOTFILE%/}"
+
+    if [[ "$dotfile" == */profiles/* ]]; then
+      profile_name="(''${dotfile##*/profiles/}) "
+    else
+      # no profile selected, no profile name to display
+      profile_name=""
+    fi
+
+    # use https://bash-prompt-generator.org to change this
+    PS1_CMD1=$(git branch --show-current 2>/dev/null); 
+    PS1='\[\e[38;5;202;1m\]''${profile_name}\[\e[0m\][\[\e[38;5;81m\]\u\[\e[0m\]@\[\e[38;5;81m\]\h\[\e[0m\]:\[\e[38;5;81m\]\w\[\e[0m\]] \[\e[38;5;76;3m\]''${PS1_CMD1}\[\e[0m\] \\$ '
+  '';
 }
