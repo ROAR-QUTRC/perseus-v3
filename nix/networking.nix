6bf94bdd8f0b669934278e5f44a7f1f6a06c4924
DanB <jdbretherton12@gmail.com>
2026-09-01 19:44:38 +1000

    feat: Added zenoh basic configs

--- nix/networking.nix
+++ nix/networking.nix
@@ -28,5 +28,8 @@
 
     # use Zenoh ROS middleware
     export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+
+    export ZENOH_SESSION_CONFIG_URI="${../software/networking/zenoh_session_config.json5}"
+    export ZENOH_ROUTER_CONFIG_URI="${../software/networking/zenoh_router_config.json5}"
   '';
 }
