d7ab23ab416160409cbfe3199806ede3759d93d5
BrockoliniMorgan <brockjamesmorgan@gmail.com>
2026-09-01 23:26:18 +1000

    Feat: Refactor of excavation firmware to multiple files

---
+++ firmware/excavation-bucket/include/motor_driver.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <board_support.hpp>
+
+class MotorDriver {
+public:
+  enum class direction { FORWARD, STOPPED, BACKWARD };
+
+  MotorDriver(const bsp::pin_pair_t &pins);
+  virtual ~MotorDriver();
+
+  void set_speed(int16_t speed);
+
+private:
+  direction _prev_direction = direction::STOPPED;
+
+  bsp::pin_pair_t _pins;
+};
