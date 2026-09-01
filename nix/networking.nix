90023de2264616055dac22e56ddc736fc6a5a210
DanB <jdbretherton12@gmail.com>
2026-09-01 20:33:23 +1000

    feat: Debugging zenoh config

--- nix/networking.nix
+++ nix/networking.nix
@@ -29,7 +29,11 @@
     # use Zenoh ROS middleware
     export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 
+    # our custom Zenoh configs
     export ZENOH_SESSION_CONFIG_URI="${../software/networking/zenoh_session_config.json5}"
     export ZENOH_ROUTER_CONFIG_URI="${../software/networking/zenoh_router_config.json5}"
+
+    # debug for Zenoh
+    export RUST_LOG=zenoh=debug
   '';
 }
