4e5333cec53e2e08bddf61cddcabf62b17bdc3f8
BrockoliniMorgan <brockjamesmorgan@gmail.com>
2026-09-04 17:32:17 +1000

    Fix: Re-added colcon clean

--- nix/extra-packages/colcon/overlay.nix
+++ nix/extra-packages/colcon/overlay.nix
@@ -2,7 +2,11 @@ final: prev:
 let
   # colcon with output filtered with grep to suppress the BARRAGE of warnings from colcon about the ament prefix path
   # Unfortunately, they're just a side effect of the nix ros build system
-  colcon = final.callPackage ./colcon-no-warnings.nix { inherit (prev) colcon; };
+  colcon = final.callPackage ./colcon-no-warnings.nix {
+    colcon = prev.colcon.withExtensions [
+      final.python3Packages.colcon-clean
+    ];
+  };
   # mini package which puts COLCON_IGNORE in the output result folder
   # allows colcon build of workspace after run nix build
   colcon-ignore = final.callPackage ./colcon-ignore.nix { };
