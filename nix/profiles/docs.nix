7e6db3539c7820df44361f4cc2affccb1ab887fe
huon <huonswales@gmail.com>
2026-09-10 10:46:49 +1000

    Chore: cleaup

--- nix/profiles/docs.nix
+++ nix/profiles/docs.nix
@@ -24,7 +24,6 @@ in
   packages = [
     docs-with-plugins
     pkgs-docs.drawio # still need the drawio binary for mkdocs-drawio-exporter
-    pkgs-docs.xvfb-run # for headless drawio
   ];
   languages = {
     python.enable = true;
@@ -32,6 +31,6 @@ in
 
   scripts = {
     dev.exec = "cd $(git rev-parse --show-toplevel)/docs && mkdocs serve";
-    build.exec = "cd $(git rev-parse --show-toplevel)/docs && xvfb-run -a mkdocs build";
+    build.exec = "cd $(git rev-parse --show-toplevel)/docs && mkdocs build";
   };
 }
